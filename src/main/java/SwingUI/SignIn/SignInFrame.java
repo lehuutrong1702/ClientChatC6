5d73eb2dfd951e1481644b751fc31f659ca350e1
21127360 <pvminh21@clc.fitus.edu.vn>
2023-11-30 12:57:48 +0700

    update login

--- src/main/java/SwingUI/SignIn/SignInFrame.java
+++ src/main/java/SwingUI/SignIn/SignInFrame.java
@@ -16,34 +16,20 @@ import java.awt.Insets;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Objects;
 
 public class SignInFrame extends JFrame {
     private JLabel mainLabel;
-    private JButton loginButton;
-    private JTextField username;
-    private JPasswordField password;
+    private JButton bSignIn;
+    private JButton bSignUp;
+    private JTextField tfUsername;
+    private JPasswordField pfPassword;
 
     public SignInFrame() throws IOException {
         setTitle("Sign In");
         mainLabel = new JLabel("SIGN IN");
-        username = new JTextField() {
-            protected void paintComponent(Graphics g) {
-                if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
-                    Graphics2D g2 = (Graphics2D) g.create();
-                    g2.setPaint(getBackground());
-                    g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
-                            0, 0, getWidth() - 1, getHeight() - 1));
-                    g2.dispose();
-                }
-                super.paintComponent(g);
-            }
-            public void updateUI() {
-                super.updateUI();
-                setOpaque(false);
-                setBorder(new RoundedCornerBorder());
-            }
-        };
-        password = new JPasswordField() {
+
+        tfUsername = new JTextField() {
             protected void paintComponent(Graphics g) {
                 if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
                     Graphics2D g2 = (Graphics2D) g.create();
@@ -61,7 +47,43 @@ public class SignInFrame extends JFrame {
             }
         };
 
-        loginButton = new JButton("Sign In") {
+        pfPassword = new JPasswordField() {
+            protected void paintComponent(Graphics g) {
+                if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+                    Graphics2D g2 = (Graphics2D) g.create();
+                    g2.setPaint(getBackground());
+                    g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+                            0, 0, getWidth() - 1, getHeight() - 1));
+                    g2.dispose();
+                }
+                super.paintComponent(g);
+            }
+            public void updateUI() {
+                super.updateUI();
+                setOpaque(false);
+                setBorder(new RoundedCornerBorder());
+            }
+        };
+
+        bSignIn = new JButton("Sign in") {
+            protected void paintComponent(Graphics g) {
+                if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+                    Graphics2D g2 = (Graphics2D) g.create();
+                    g2.setPaint(getBackground());
+                    g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+                            0, 0, getWidth() - 1, getHeight() - 1));
+                    g2.dispose();
+                }
+                super.paintComponent(g);
+            }
+            public void updateUI() {
+                super.updateUI();
+                setOpaque(false);
+                setBorder(new RoundedCornerBorder());
+            }
+        };
+
+        bSignUp = new JButton("Create new account") {
             protected void paintComponent(Graphics g) {
                 if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
                     Graphics2D g2 = (Graphics2D) g.create();
@@ -80,56 +102,61 @@ public class SignInFrame extends JFrame {
         };
 
         setContentPane(new JPanel() {
-            BufferedImage bufferedImage = ImageIO.read(SignInFrame.class.getClassLoader().getResourceAsStream("background.jpg"));
+            BufferedImage bufferedImage = ImageIO.read(Objects.requireNonNull(SignInFrame.class.getClassLoader().getResourceAsStream("background.jpg")));
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.drawImage(bufferedImage,0,0,this);
             }
         });
+
         init();
     }
 
     public void addEventListeners() {
         //submit button action listener
-        loginButton.addActionListener(new ActionListener() {
+        bSignIn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String data = "Username: " + username.getText();
-                data += "\n" + "Password: " + password.getText();
-                JOptionPane.showMessageDialog(null, data);
+
             }
         });
 
-        username.addFocusListener(new FocusListener() {
+        bSignUp.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        tfUsername.addFocusListener(new FocusListener() {
             public void focusLost(FocusEvent e) {
-                if(username.getText().equals("")) {
-                    username.setText("Enter your username");
-                    username.setForeground(Color.gray);
+                if(tfUsername.getText().isEmpty()) {
+                    tfUsername.setText("Enter your username");
+                    tfUsername.setForeground(Color.gray);
                 }
             }
 
             public void focusGained(FocusEvent e) {
-                if(username.getText().equals("Enter your username")) {
-                    username.setText("");
-                    username.setForeground(Color.black);
+                if(tfUsername.getText().equals("Enter your username")) {
+                    tfUsername.setText("");
+                    tfUsername.setForeground(Color.black);
                 }
             }
         });
 
-        password.addFocusListener(new FocusListener() {
+        pfPassword.addFocusListener(new FocusListener() {
 
             public void focusLost(FocusEvent e) {
-                if(password.getText().equals("")) {
-                    password.setText("Enter your password");
-                    password.setForeground(Color.gray);
-                    password.setEchoChar((char)0);
+                if(pfPassword.getText().equals("")) {
+                    pfPassword.setText("Enter your password");
+                    pfPassword.setForeground(Color.gray);
+                    pfPassword.setEchoChar((char)0);
                 }
             }
 
             public void focusGained(FocusEvent e) {
-                if(password.getText().equals("Enter your password")) {
-                    password.setText("");
-                    password.setEchoChar('*');
-                    password.setForeground(Color.black);
+                if(pfPassword.getText().equals("Enter your password")) {
+                    pfPassword.setText("");
+                    pfPassword.setEchoChar('*');
+                    pfPassword.setForeground(Color.black);
                 }
             }
         });
@@ -139,24 +166,29 @@ public class SignInFrame extends JFrame {
         mainLabel.setFont(new Font("Arial", Font.BOLD, 60));
         mainLabel.setForeground(Color.white);
 
-        username.setPreferredSize(new Dimension(250,35));
-        password.setPreferredSize(new Dimension(250,35));
+        tfUsername.setPreferredSize(new Dimension(250,35));
+        pfPassword.setPreferredSize(new Dimension(250,35));
 
-        loginButton.setPreferredSize(new Dimension(250,35));
-        loginButton.setBackground(new Color(66, 245, 114));
-        loginButton.setFocusPainted(false);
+        bSignIn.setPreferredSize(new Dimension(250,35));
+        bSignIn.setBackground(new Color(66, 245, 114));
+        bSignIn.setFocusPainted(false);
 
-        username.setText("Enter your username");
-        username.setForeground(Color.gray);
-        password.setText("Enter your password");
-        password.setForeground(Color.gray);
-        password.setEchoChar((char)0);
+        bSignUp.setPreferredSize(new Dimension(250,35));
+        bSignUp.setBackground(new Color(66, 245, 114));
+        bSignUp.setFocusPainted(false);
+
+        tfUsername.setText("Enter your username");
+        tfUsername.setForeground(Color.gray);
+        pfPassword.setText("Enter your password");
+        pfPassword.setForeground(Color.gray);
+        pfPassword.setEchoChar((char)0);
 
         setLayout(new GridBagLayout());
 
-        Insets textInsets = new Insets(10, 10, 5, 10);
-        Insets buttonInsets = new Insets(20, 10, 10, 10);
         Insets labelInsets = new Insets(-200, 10, 0, 10);
+        Insets textInsets = new Insets(10, 10, 5, 10);
+        Insets bSignInInsets = new Insets(20, 10, 10, 10);
+        Insets bSignUpInsets = new Insets(0, 10, 0, 10);
 
         GridBagConstraints input = new GridBagConstraints();
         input.anchor = GridBagConstraints.CENTER;
@@ -167,18 +199,24 @@ public class SignInFrame extends JFrame {
         input.anchor = GridBagConstraints.CENTER;
         input.insets = textInsets;
         input.gridy = 3;
-        add(username,input);
+        add(tfUsername,input);
 
         input.insets = textInsets;
         input.anchor = GridBagConstraints.CENTER;
         input.gridy = 5;
-        add(password,input);
+        add(pfPassword,input);
 
-        input.insets = buttonInsets;
+        input.insets = bSignInInsets;
         input.anchor = GridBagConstraints.WEST;
         input.gridx = 0;
         input.gridy = 7;
-        add(loginButton,input);
+        add(bSignIn,input);
+
+        input.insets = bSignUpInsets;
+        input.anchor = GridBagConstraints.WEST;
+        input.gridx = 0;
+        input.gridy = 8;
+        add(bSignUp,input);
 
         setSize(950,650);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
