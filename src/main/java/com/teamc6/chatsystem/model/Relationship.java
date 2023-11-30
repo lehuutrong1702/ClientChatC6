dadd771903fd1d63969b2f70914f9dce6641803a
nguyenpv.30@gmail.com <126704701+nguyen30042003@users.noreply.github.com>
2023-11-30 13:01:34 +0700

    update code service 1

---
+++ src/main/java/com/teamc6/chatsystem/model/Relationship.java
@@ -0,0 +1,27 @@
+package com.teamc6.chatsystem.model;
+
+public class Relationship {
+    private long relationshipId;
+    private String relationshipName;
+
+    public Relationship(long relationshipId, String relationshipName) {
+        this.relationshipId = relationshipId;
+        this.relationshipName = relationshipName;
+    }
+
+    public long getRelationshipId() {
+        return relationshipId;
+    }
+
+    public void setRelationshipId(long relationshipId) {
+        this.relationshipId = relationshipId;
+    }
+
+    public String getRelationshipName() {
+        return relationshipName;
+    }
+
+    public void setRelationshipName(String relationshipName) {
+        this.relationshipName = relationshipName;
+    }
+}
